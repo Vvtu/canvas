0f509cbb8cfd2a3c638f9a63f1952c9c732f62be
Viacheslav Tuev <vvtuev@gmail.com>
2021-02-07 16:42:29 +0300

    fix line

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -8,102 +8,82 @@ type Coordinate = {
   x: number,
   y: number,
 };
-const LINES = 10;
+
+const distance = (a: Coordinate, b: Coordinate) =>
+  Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+
+const SCALE = 60;
 
 const Canvas = ({ width, height }: CanvasProps) => {
-  console.log("width = ", width); //TODO vvtu, need to delete
-  console.log("height = ", height); //TODO vvtu, need to delete
   const canvasRef = useRef();
   const canvasRefBase = useRef();
 
   const [isPainting, setIsPainting] = useState(false);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  const SCALE = Math.min(width, height) / LINES;
-  console.log("SCALE = ", SCALE); //TODO vvtu, need to delete
+  const [point1, setPoint1] = useState({ x: -2, y: -2 });
+  const [point2, setPoint2] = useState({ x: 2, y: 2 });
 
-  const toReal = ({ x, y }: Coordinate) => ({
-    x: x * SCALE + width / 2,
-    y: -y * SCALE + height / 2,
-  });
-  const fromReal = ({ x, y }: Coordinate) => ({
-    x: (x - width / 2) / SCALE,
-    y: -(y - height / 2) / SCALE,
-  });
+  const toReal = useCallback(
+    ({ x, y }: Coordinate) => ({
+      x: x * SCALE + width / 2,
+      y: -y * SCALE + height / 2,
+    }),
+    [height, width]
+  );
+  const fromReal = useCallback(
+    ({ x, y }: Coordinate) => ({
+      x: (x - width / 2) / SCALE,
+      y: -(y - height / 2) / SCALE,
+    }),
+    [height, width]
+  );
 
-  console.log(fromReal(toReal({ x: 1, y: 1 })));
+  const updateLineWithNewCoordinates = useCallback(
+    (event: MouseEvent): Coordinate | undefined => {
+      if (!canvasRef.current) {
+        return;
+      }
+      if (isPainting === false) {
+        return;
+      }
+      const canvas: HTMLCanvasElement = canvasRef.current;
 
-  const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
-    if (!canvasRef.current) {
-      return;
-    }
-    const canvas: HTMLCanvasElement = canvasRef.current;
-    return {
-      x: event.pageX - canvas.offsetLeft,
-      y: event.pageY - canvas.offsetTop,
-    };
-  };
-
-  const startPaint = useCallback((event: MouseEvent) => {
-    const coordinates = getCoordinates(event);
-    if (coordinates) {
-      setMousePosition(coordinates);
-      setIsPainting(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!canvasRef.current) {
-      return;
-    }
-    const canvas: HTMLCanvasElement = canvasRef.current;
-    canvas.addEventListener("mousedown", startPaint);
-    return () => {
-      canvas.removeEventListener("mousedown", startPaint);
-    };
-  }, [startPaint]);
-
-  const paint = useCallback(
-    (event: MouseEvent) => {
-      if (isPainting) {
-        const newMousePosition = getCoordinates(event);
-        if (mousePosition && newMousePosition) {
-          drawLine(mousePosition, newMousePosition);
-          setMousePosition(newMousePosition);
+      const coordinate = fromReal({
+        x: event.pageX - canvas.offsetLeft,
+        y: event.pageY - canvas.offsetTop,
+      });
+      const d1 = distance(coordinate, point1);
+      const d2 = distance(coordinate, point2);
+      if (d1 < d2 && d1 < 1) {
+        setPoint1(coordinate);
+      } else {
+        if (d2 < d1 && d2 < 1) {
+          setPoint2(coordinate);
         }
       }
     },
-    [isPainting, mousePosition]
+    [fromReal, isPainting, point1, point2]
   );
 
-  useEffect(() => {
-    if (!canvasRef.current) {
-      return;
-    }
-    const canvas: HTMLCanvasElement = canvasRef.current;
-    canvas.addEventListener("mousemove", paint);
-    return () => {
-      canvas.removeEventListener("mousemove", paint);
-    };
-  }, [paint]);
-
-  const exitPaint = useCallback(() => {
-    setIsPainting(false);
-    setMousePosition(undefined);
-  }, []);
+  const mouseUp = () => setIsPainting(false);
+  const mouseDown = () => setIsPainting(true);
 
   useEffect(() => {
     if (!canvasRef.current) {
       return;
     }
     const canvas: HTMLCanvasElement = canvasRef.current;
-    canvas.addEventListener("mouseup", exitPaint);
-    canvas.addEventListener("mouseleave", exitPaint);
+    canvas.addEventListener("mousedown", mouseDown);
+    canvas.addEventListener("mousemove", updateLineWithNewCoordinates);
+    canvas.addEventListener("mouseup", mouseUp);
+    // canvas.addEventListener("mouseleave", mouseDown);
     return () => {
-      canvas.removeEventListener("mouseup", exitPaint);
-      canvas.removeEventListener("mouseleave", exitPaint);
+      canvas.removeEventListener("mousedown", mouseDown);
+      canvas.removeEventListener("mousemove", updateLineWithNewCoordinates);
+      canvas.removeEventListener("mouseup", mouseUp);
+      // canvas.removeEventListener("mouseleave", mouseDown);
     };
-  }, [exitPaint]);
+  }, [updateLineWithNewCoordinates, setIsPainting]);
 
   useEffect(() => {
     if (!canvasRefBase.current) {
@@ -112,69 +92,68 @@ const Canvas = ({ width, height }: CanvasProps) => {
     const canvasBase: HTMLCanvasElement = canvasRefBase.current;
     const contextBase = canvasBase.getContext("2d");
     if (contextBase) {
-      const max = fromReal({ x: width, y: height });
-      const min = fromReal({ x: 0, y: 0 });
-      console.log("max = ", max); //TODO vvtu, need to delete
-      console.log("min = ", min); //TODO vvtu, need to delete
       contextBase.strokeStyle = "#333";
       contextBase.lineWidth = 1;
 
       contextBase.beginPath();
 
-      for (let x = -LINES; x < LINES; x += 1) {
+      const lines = Math.round(
+        Math.max(canvasBase.height, canvasBase.width) / SCALE
+      );
+
+      for (let x = -lines; x < lines; x += 1) {
         const from = toReal({ x, y: 0 });
         const to = toReal({ x, y: 0 });
         contextBase.moveTo(from.x, 0);
-        contextBase.lineTo(to.x, height);
+        contextBase.lineTo(to.x, canvasBase.height);
       }
-      for (let y = -LINES; y < LINES; y += 1) {
+      for (let y = -lines; y < lines; y += 1) {
         const from = toReal({ x: 0, y });
         const to = toReal({ x: 0, y });
         contextBase.moveTo(0, from.y);
-        contextBase.lineTo(width, to.y);
+        contextBase.lineTo(canvasBase.width, to.y);
       }
       contextBase.closePath();
       contextBase.stroke();
 
       contextBase.strokeStyle = "#555";
-      contextBase.lineWidth = 2;
+      contextBase.lineWidth = 1;
       contextBase.beginPath();
-      const from = toReal({ x: -LINES, y: 0 });
-      const to = toReal({ x: LINES, y: 0 });
+      const from = toReal({ x: -lines, y: 0 });
+      const to = toReal({ x: lines, y: 0 });
       contextBase.moveTo(from.x, from.y);
       contextBase.lineTo(to.x, to.y);
 
-      const from2 = toReal({ x: 0, y: -LINES });
-      const to2 = toReal({ x: 0, y: LINES });
+      const from2 = toReal({ x: 0, y: -lines });
+      const to2 = toReal({ x: 0, y: lines });
       contextBase.moveTo(from2.x, from2.y);
       contextBase.lineTo(to2.x, to2.y);
       contextBase.closePath();
       contextBase.stroke();
     }
-  }, [height, width, canvasRefBase.current]);
+  }, [toReal]);
 
-  const drawLine = (
-    originalMousePosition: Coordinate,
-    newMousePosition: Coordinate
-  ) => {
+  useEffect(() => {
     if (!canvasRef.current) {
       return;
     }
     const canvas: HTMLCanvasElement = canvasRef.current;
     const context = canvas.getContext("2d");
     if (context) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
       context.strokeStyle = "red";
-      context.lineJoin = "round";
-      context.lineWidth = 2;
-
+      context.lineWidth = 1;
       context.beginPath();
-      context.moveTo(originalMousePosition.x, originalMousePosition.y);
-      context.lineTo(newMousePosition.x, newMousePosition.y);
-      context.closePath();
 
+      const from = toReal(point1);
+      const to = toReal(point2);
+      context.moveTo(from.x, from.y);
+      context.lineTo(to.x, to.y);
+      context.closePath();
       context.stroke();
     }
-  };
+  }, [point1, point2, toReal]);
 
   return (
     <>
